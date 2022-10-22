2779119551e20f917a80bf69ba0d96a8f61e7899
lazysomedayz <lazysomedayzz@hotmail.com>
2022-10-23 00:34:00 +0700

    Add project files.

---
+++ Exersise4-2/Dice.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exersise4_2
+{
+    internal class Dice
+    {
+        Random random = new Random();
+        public int Throw()
+        {
+            return random.Next(1, 6);
+        }
+    }
+}
---
+++ Exersise4-2/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Exersise4_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dice dic1 = new Dice();
+            Dice dic2 = new Dice();
+            SnakeEyes(dic1, dic2);
+        }
+
+        static void SnakeEyes(Dice dice1, Dice dice2)
+        {
+            int mark1 = 0, mark2 = 0, i = 1;
+            while (!(mark1 == 1 && mark2 == 1))
+            {
+                mark1 = dice1.Throw();
+                mark2 = dice2.Throw();
+                Console.WriteLine(" {0} Mark of Dices : {1},{2} ", i, mark1, mark2);
+                System.Threading.Thread.Sleep(10);
+                i = i + 1;
+            }
+            Console.WriteLine("Throw of both dices = 1 is : " + (i - 1) + " time ");
+            Console.ReadLine();
+        }
+    }
+}
